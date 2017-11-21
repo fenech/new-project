72b07066f3cefa44debb92e01f5ddd9586c20136
Tom Fenech <tomjwfenech@gmail.com>
2017-11-21 11:15:04 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: {
+        app: "./src/index.tsx",
+        vendor: [
+            "react",
+            "react-dom"
+        ]
+    },
+    output: {
+        path: path.join(__dirname, "dist"),
+        publicPath: "/",
+        filename: "[name].bundle.js"
+    },
+    devServer: {
+        historyApiFallback: true,
+        headers: {
+            "Access-Control-Allow-Origin": "*"
+        },
+        proxy: {
+        }
+    },
+    watchOptions: {
+        aggregateTimeout: 300,
+        poll: 3000,
+        ignored: /node_modules/
+    },
+    resolve: {
+        extensions: [".webpack.js", ".web.js", ".js", ".ts", ".tsx", ".css"]
+    },
+    devtool: "eval-source-map",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: [
+                    "babel-loader",
+                    "ts-loader"
+                ]
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    "style-loader",
+                    {
+                        loader: "typings-for-css-modules-loader",
+                        query: {
+                            modules: true,
+                            namedExport: true,
+                            camelCase: true,
+                            localIdentName: "[name]__[local]___[hash:base64:5]"
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin("vendor"),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "index.html")
+        })
+    ]
+};
